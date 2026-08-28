e19ea4151b9c4fc428b6bba1bdabc4409eb5fdab
Owen Powell <opowell2899@gmail.com>
2026-08-28 12:48:40 -0400

    Filter get_leagues to only return AL and NL

--- commands.py
+++ commands.py
@@ -1,19 +1,21 @@
 import requests
 import json
 
-base_url = 'https://statsapi.mlb.com/api/v1/'
-
 def get_leagues():
-    url = base_url + 'leagues'
+    # This url responds with just MLB leagues
+    url = 'https://statsapi.mlb.com/api/v1/leagues?sportId=1'
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
 
         leagues = data["leagues"]
-        first_league = leagues[0]
 
-        print(f'League id: {first_league["id"]} | League name: {first_league["name"]}')
+        for league in leagues:
+            # filter by AL and NL
+            if league["id"] == 103 or league["id"] == 104:
+                print(f'League id: {league["id"]} | League name: {league["name"]}')
+
         return None
     else:
         print(f'{response.status_code}')
